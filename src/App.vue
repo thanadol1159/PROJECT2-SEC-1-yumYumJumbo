e74a9e1272b44274f2ca92cd23f2b1e6303c2656
Nach <benten599@hotmail.com>
2023-03-16 22:16:49 +0700

    comment app

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,19 @@
 <script setup>
-import Navbar from "./components/Navbar.vue";
-import ContentSection from "./components/ContentSection.vue";
-import Carousel from "./components/Carousel.vue";
-import { getData } from "./composable/getData.js"
+// import Navbar from "./components/Navbar.vue";
+// import ContentSection from "./components/ContentSection.vue";
+// import Carousel from "./components/Carousel.vue";
+// import { getData } from "./composable/getData.js"
 
-const items = getData()
-// const arr = items.slice(3,7)
+// const items = getData()
+// const randomPreview = () => {
+//     const fLength = Math.floor(Math.random() * items.length)
+//     const lLength = fLength + 3 + Math.ceil(Math.random() * 6)
 
-
-// const fLength = Math.floor(Math.random() * items.length)
-// const lLength = Math.floor(Math.random() * items.length)
-// console.log(fLength)
-// console.log(lLength)
-console.log(items)
-const randomPreview = () => {
-    const fLength = Math.floor(Math.random() * items.length)
-    const lLength = fLength + 3 + Math.ceil(Math.random() * 6)
-
-    if (lLength > items.length) {
-      fLength - 7
-    }
-    console.log(fLength)
-    console.log(lLength)
-    return items.slice(fLength, lLength)
-}
+//     if (lLength > items.length) {
+//       fLength - 7
+//     }
+//     return items.slice(fLength, lLength)
+// }
 </script>
 <template>
   <!-- <Navbar />
