73691f55f417629ce935f19a9b397e211cc7135f
Nach <benten599@hotmail.com>
2023-03-17 17:07:05 +0700

    push navbar & carousel

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,47 @@
 <script setup>
+import Category from "./components/Category.vue";
+import PopuparItem from "./components/PopularItem.vue";
+import Buying from "./components/Buying.vue";
+import Cart from "./components/Cart.vue";
+import PaymentMethod from "./components/PaymentMethod.vue";
+import Carousel from "./components/Carousel.vue";
+import Navbar from "./components/Navbar.vue";
+import ContentSection from "./components/ContentSection.vue";
+import { getData } from "./composable/getData.js";
+const items = getData();
 
-import Category from './components/Category.vue';
-import PopuparItem from './components/PopularItem.vue'; 
-import Buying from './components/Buying.vue';
-import Cart from './components/Cart.vue';
-import PaymentMethod from './components/PaymentMethod.vue';
-import Carousel from './components/Carousel.vue';
-import Navbar from './components/Navbar.vue';
-import ContentSection from './components/ContentSection.vue';
-import { getData } from './composable/getData';
-
+const randomPreview = () => {
+  const fLength = Math.floor(Math.random() * items.length);
+  const lLength = fLength + 2 + Math.ceil(Math.random() * 4);
+  return items.slice(fLength, lLength);
+};
 </script>
 
 <template>
-<div>
+  <div>
     <div>
-        <Navbar/>
-        <ContentSection>
-            <Carousel >
+      <Navbar />
+      <ContentSection>
+        <Carousel :item-list="randomPreview()" :full="true"> </Carousel>
+      </ContentSection>
 
-            </Carousel>
-        </ContentSection>
-        <!-- <Category />
-        <PopuparItem /> -->
-        <!-- <Buying /> -->
-        <!-- <Cart />
+      <ContentSection >
+        <Category />
+      </ContentSection>
+      <ContentSection>
+        <PopuparItem />
+      </ContentSection>
+
+      <!-- <Buying />
+      <Cart />
         <PaymentMethod /> -->
-        
     </div>
-
-</div>
+  </div>
 </template>
 
 <style scoped>
-*{
-    font-family: 'Kanit', 'sans-serif';
-    font-size: 36px;
+* {
+  font-family: "Kanit", "sans-serif";
+  font-size: 36px;
 }
-
-
 </style>
