3086a40d659ad586920d704f87e67bc2c8b0c85f
Pichaya Chantrasriwong <88099446+StreetK9@users.noreply.github.com>
2023-03-18 16:39:24 +0700

    Create TopSaleItem.vue

---
+++ src/components/TopSaleItem.vue
@@ -0,0 +1,72 @@
+<script setup>
+import { ref } from "vue";
+import { getData } from '../composable/getData.js'
+
+// const datas = ref(getData())
+
+const datas = ref(getData().sort((a, b) => b.rating.count - a.rating.count))
+
+console.log(datas.value);
+
+</script>
+ 
+<template>
+    <div class="pop border border-black w-9/12 m-auto rounded-lg truncate mt-8">
+        <h1 class="text-4xl mt-2 pl-10 text-white ">Top Sale</h1>
+        <div class="flex overflow-y-auto mt-2 pb-2">
+            <div v-for="data in datas" :key="data.id">
+                <div v-show="data.rating.count > 5000">
+                    <div class="cursor-pointer bg-white h-60 w-48 rounded-2xl mx-3 shadow drop-shadow-2xl border border-black hover:border-red-500 hover:shadow-2xl hover:border-2 overflow-hidden "
+                        v-on:click="">
+                        <img class="m-auto w-40 mt-4 border rounded-lg border-black shadow shadow-violet-600"
+                            :src="data.images" />
+                        <p
+                            class="text-xs pl-5 mt-1 bg-slate-500 border border-red-600 border-l-0 border-r-0 text-slate-200">
+                            ขายเเล้ว : <span class="countData">{{
+                                data.rating.count.toLocaleString()
+                            }}</span></p>
+                        <div class="mt-1 bg-black h-12">
+                            <h1 class="text-xs px-5 text-white py-2 truncate ">{{
+                                data.name }}
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+ 
+<style scoped>
+.pop {
+    background-color: #754A8F;
+    height: 320px;
+}
+
+.countData {
+    color: #fddf00;
+    text-shadow: 0 0 3px rgb(0, 0, 0);
+}
+
+::-webkit-scrollbar {
+    width: 5px;
+}
+
+::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 5px grey;
+    border-radius: 45px;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+    background: #9263B1;
+    border: 1px solid rgb(135, 52, 116);
+    border-radius: 400px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+    background: #9263B1;
+    border: 1px solid rgb(255, 255, 255);
+}
+</style>
