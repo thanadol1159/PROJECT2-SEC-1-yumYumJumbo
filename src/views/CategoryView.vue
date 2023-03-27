33e49014c5cd1961ad2914d7aea4b93270de58d4
Nach <benten599@hotmail.com>
2023-03-27 20:26:18 +0700

    up

---
+++ src/views/CategoryView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from "vue";
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+<div>
+    <button class="text-9xl m-auto"> ??? </button>
+</div>
+</template>
+ 
+<style scoped>
+
+</style>
