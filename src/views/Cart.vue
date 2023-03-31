77ecb5dce65673eabbaa13a2c42b4a0e609700d8
PakapolTaubol <pakapol.taubol@mail.kmutt.ac.th>
2023-03-31 23:52:40 +0700

    Update Cart.vue

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -4,46 +4,20 @@ import Ordering from "../components/Buying/Ordering.vue";
 import Navbar from "../components/Navbar.vue";
 import ProductDetail from "./ProductDetail.vue";
 
-
-// const test = [
-//   {
-//     id: 1,
-//     name: "LOOKER - เสื้อยืดสกรีนนูน",
-//     price: 200,
-//     description:
-//       "ฟรีไซต์ อก36-44ใส่ได้ ความยาวประมาณ29นิ้วครับ สินค้าตามภาพ ไม่มีป้ายด้านล่าง",
-//     category: "men's clothing",
-//     type: ["เสื้อยืด", "เสื้อ oversize"],
-//     images: [
-//       "https://cf.shopee.co.th/file/sg-11134201-23010-we1oph91bwlv7c",
-//       "https://cf.shopee.co.th/file/sg-11134201-23010-e1hfmnl2bwlv45",
-//       "https://cf.shopee.co.th/file/sg-11134201-23010-rjvcgol2bwlv78",
-//     ],
-//     rating: {
-//       rate: 4.8,
-//       count: 1145,
-//     },
-//   },
-//   {
-//     id: 2,
-//     name: "เสื้อยืดชาย oversize คอกลม แขนสั้น",
-//     price: 85,
-//     description: "เนื้อนุ่ม ใส่สบาย ระบายความร้อนดีเยี่ยม ผ้าไม่ติดตัว",
-//     category: "men's clothing",
-//     type: ["เสื้อยืด", "เสื้อ oversize"],
-//     images: [
-//       "https://cf.shopee.co.th/file/21a5def22b5927960e827b48940d52eb",
-//       "https://cf.shopee.co.th/file/df888b6513b63a57b20805f94da32b2f",
-//     ],
-//     rating: {
-//       rate: 4.6,
-//       count: 689,
-//     },
-//   },
-// ];
 const totals = ref(0);
 const buy = []
 const frommark = ref([]);
+
+onMounted(()=>{
+  frommark.value = props.productCart
+  totals.value = 0
+  for (const item of frommark.value) {
+    item.quantity = 1
+    totals.value += item.price
+  }
+  console.log(frommark.value);
+})
+
 const addToBuy =(item)=>{
   for (let i = 0; i < buy.length; i++) {
     buy[i].push(item);
@@ -88,11 +62,6 @@ const props = defineProps({
     }
 });
 
-onMounted(()=>{
-  console.log(props.productCart);
-  frommark.value = props.productCart
-})
-
 </script>
 <template>
   <!-- <Navbar/> -->
@@ -130,7 +99,7 @@ onMounted(()=>{
         </button>
       </div>
       <div class="flex text-slate-50 items-center">
-        {{ item.price }} 
+        {{ item.total_price }} 
       </div>
     </div>
     total {{ totals }}
